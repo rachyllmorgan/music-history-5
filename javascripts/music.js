2567d2018dbb9a3b3f9a90a14021efd172dcec43
Rachel Morgan <rachyllmorgan@gmail.com>
2015-08-11 19:18:31 -0500

    trying to populate menus

--- javascripts/music.js
+++ javascripts/music.js
@@ -1,107 +1,91 @@
-// *****************Music History 2 ******************
-
-// var songs = [];
-// var list_of_songs = document.getElementById("songlist");
-
-// songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-// songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-// songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-// songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-// songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-
-
-// var addSongBottom = songs.push("Hold Back the River > by James Bay on the album Chaos and the Calm");
-// var addSongTop = songs.unshift("Just One Drink > by Jack White on the album Lazaretto");
-
-//var addSong = prompt("Pick a song");
-//	if (addSong===""); {
-//var addArtist = prompt("Who sings it?")
-//	}
-//	if (addSong===""); {
-//var addAlbum = prompt("On what album?")
-//	songs.push(addSong + " &#8208 by " + addArtist + " on the album " + addAlbum);
-//}
-
-// console.log(song_mod);
-// list_of_songs.innerHTML += "<div class='list_mod'>" + song_mod + "</div>" + "<br>";
-// }
-
-// ************* Music History 4 *****************
-
-//$(document).ready(function(){	
-//	
-//		$.ajax({
-//				url: "/javascripts/music.json",
-//		}).done(function(data) {
-//			var table = '';
-//		for(var i =0; i <= data.songs.length; i++) {
-//			console.log(data.songs[i]);
-//			table += "<li class='tracks'><h4>"+data.songs[i].name+"</h4></br>"+data.songs[i].artist+"</br>"+data.songs[i].album+"</br></li>"+"<button type='button' class='remove'>Remove</button></br></br>";
-//			$('.list-group').html(table);
-//		}
-//						});
-//		$(".more").click(function(){
-//			$.ajax({
-//				url: "/javascripts/music2.json",
-//			}).done(function(data) {
-//			var table = '';
-//		for(var i =0; i <= data.songs.length; i++) {
-//			table += "<h4>"+data.songs[i].name+"</h4></br>"+data.songs[i].artist+"</br>"+data.songs[i].album+"</br>"+"<button type='button' class='remove'>Remove</button></br></br>";
-//			
-//			console.log(data.songs[i]);
-//			
-//			$('.list-group').append(table);
-//		}
-//						});
-//		});
-//	
-//	$(document).on("click", ".tracks", function(){
-//		$(this).parent.remove();
-//	});
-//	
-//	$(".form-control").val(function(){
-//		
-//	});
-//		
-//});
-
 // **************** Music History 5 *******************
 
 requirejs.config({
   baseUrl: './javascripts',
   paths: {
     'jquery': '../bower_components/jquery/dist/jquery.min',
+		'firebase': '../bower_components/firebase/firebase',
+		'lodash': '../bower_components/lodash/lodash',
     'hbs': '../bower_components/require-handlebars-plugin/hbs',
     'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap.min'
   },
   shim: {
-    'bootstrap': ['jquery']
-  }
+    'bootstrap': ['jquery'],
+		'firebase': {
+			exports: 'Firebase'
+		}
+	}
 });
 
 requirejs(
-["jquery", "hbs", "bootstrap", "populate-songs", "get-more-songs"],
-	function ($, Handlebars, bootstrap, popSongs, moreSongs) {
+["jquery", "lodash", "firebase", "hbs", "bootstrap", "populate-songs", "get-more-songs"],
+	function ($, _, _firebase, Handlebars, bootstrap, popSongs, moreSongs) {
 		
- popSongs.getSongs(function(songs) {
- 			require(['hbs!../templates/songs'], function(songTemplate) {
-				console.log(popSongs);
-		$(".list-group").html(songTemplate(songs));
-			});
- });
+		var myFirebaseRef = new Firebase("https://radiant-fire-6211.firebaseio.com");
 		
-	$("#moreSongs").click(function(){
-  	moreSongs.getSongs(function(songs) {
-			require(['hbs!../templates/songs'], 			function(songTemplate) {
-			console.log(moreSongs);
-				$(".list-group").append(songTemplate(songs));
-				});
- });
+		myFirebaseRef.on("value", function(snapshot) {
+  var songs = snapshot.val();
+
+
+	// Empty Array for unique
+			
+	var allSongsArray = [];
+		
+	for (var key in songs.songs) {
+      allSongsArray[allSongsArray.length] = songs.songs[key];
+      }
+		console.log("allSongsArray", allSongsArray);
+		
+		var uniqueArtist =_.chain(allSongsArray)
+				.uniq("artist")
+				.pluck("artist")
+				.value();
+		
+		var uniqueAlbum =_.chain(allSongsArray)
+				.uniq("album")
+				.pluck("album")
+				.value();
+		
+		console.log(uniqueArtist);
+		console.log(uniqueAlbum);
+			
+//	function displaySongs() {
+		require(['hbs!../templates/songs'],
+			function(songTemplate){
+				$(".list-group").html(songTemplate(songs));
+					});
+//		}
+
+	// Artist Menu Populate		
+			
+	require(['hbs!../templates/artist_menu'],
+		function(artistTemplate){	$(".artistMenu").html(artistTemplate({artist: uniqueArtist}));
+		console.log(songs.songs);
+		});												
+		
+	$(".list-group").html("<h3>Select an Artist</h3>");
+		
+	$(".dropdown-menu li").click(function(){
+		$(".albumMenu").removeClass("disabled");
 	});
-	}
-
-//	$(".artistMenu").select(function() {
 	
+		// Submit button to add music
+$(".subBtn").click(function(){
+	event.preventDefault();
+	var newSong = {
+		"name": $("#inputSongName").val(),
+		"artist": $("#inputArtist").val(),
+		"album": $("#inputAlbum").val(),
+};
+		console.log("newSong", newSong);
 	
-);  //****** Keep me on the outside
-
+		$.ajax({
+        url: "https://radiant-fire-6211.firebaseio.com/songs.json",
+			method: "POST",
+			data: JSON.stringify(newSong)
+      }).done(function(addedSong) {
+				console.log(addedSong);
+		});
+});
+		});
+	});   //****** Keep me on the outside
--- javascripts/populate-songs.js
+++ javascripts/populate-songs.js
@@ -2,7 +2,7 @@ define(["jquery"],function($) {
   return {
     getSongs: function(popSongs) {
       $.ajax({
-        url: "./javascripts/music.json",
+        url: "https://radiant-fire-6211.firebaseio.com/.json",
       }).done(function(data) {
         popSongs(data);
       });
